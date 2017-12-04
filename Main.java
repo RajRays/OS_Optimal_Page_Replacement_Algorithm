1f5187f971ef52e10b0f19215143c05eefb5d588
RajRays <Raj@Fameria.com>
2017-12-04 02:32:13 -0500

    Reformatted Code, Javadoc Comments Added, Project Complete

--- Main.java
+++ Main.java
@@ -4,58 +4,73 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *<h1>Optimal Page Replacement Algorithm</h1>
+ * <h1>Optimal Page Replacement Algorithm</h1>
  * The Raj_OS_Optimal_Page_Replacement program implements the optimal
  * page replacement algorithm on a reference string to simulate page
  * replacement within operating systems.
+ * <p>
+ * <b>Sample Reference String : 70120304230321201701</b>
  *
- *
- * @author  Rajendra Bhagroo
+ * @author Rajendra Bhagroo
  * @version 9.0
- * @see     Main
- * @see     OptimalPage
- * @since   2017-12-2
+ * @see Main
+ * @see OptimalPage
+ * @since 2017-12-2
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         OptimalPage OptimalAlgorithm = new OptimalPage();
 
         OptimalAlgorithm.initialize();
         OptimalAlgorithm.simulate();
+
     }
 
 }//END Main Class
 
 
-
-
-
 class OptimalPage {
 
-
+    /** Used To Store Numerical Values From "refString" */
     private ArrayList<Integer> refArrayList = new ArrayList<Integer>();
+
+    /** Used To Obtain Input From User */
     private Scanner keyboard = new Scanner(System.in);
+
+    /** Used To Emulate Memory Using "frameSize" */
     private String[] memArray;
+
+    /** Used As Raw Input Of Reference String From User */
     private String refString;
+
+    /** Used To Track Number Of Page Faults*/
+
     private int pageFaults;
+
+    /** Used As Raw Input Of Frame Size From User*/
     private int frameSize;
 
 
-
-
-    //Ensures Input Is Numerical
-    private String numericStringValidation(String msg){
+    /**
+     * Eliminates Need For Exception Handling As Method
+     * Ensures Input Is A Positive Numerical String.
+     * If Not, Method Loops And Asks For Valid Input.
+     *
+     * @param  msg           Message To Be Shown To User If Invalid Reference String/Frame Is Entered
+     * @return tempString    To Be Saved Within Variables "refString" , "frameSize"
+     */
+    private String numericStringValidation(String msg) {
 
         boolean isInvalid = true;
         String tempString = null;
 
-        while(isInvalid){
+        while (isInvalid) {
 
             tempString = keyboard.next();
 
-            if(!tempString.matches("[0-9]+")) {
+            if (!tempString.matches("[0-9]+")) {
                 System.out.println("Please Enter A Numerical " + msg);
                 continue;
             } else {
@@ -68,15 +83,21 @@ class OptimalPage {
 
     }
 
-
-
-
-    private ArrayList<Integer> populateArrayList(String targetString){
+    /**
+     * Takes "refString" Entered By User,
+     * Creates New ArrayList Of Type Integer,
+     * Splits "refString" Into Separate Numbers,
+     * Fills "tempArrayList" With Each Number.
+     *
+     * @param  targetString            "refString" Entered By User
+     * @return tempArrayList           Copies Contents Into "refArrayList"
+     */
+    private ArrayList<Integer> populateArrayList(String targetString) {
 
         String[] tempArray = targetString.split("");
         ArrayList<Integer> tempArrayList = new ArrayList<Integer>();
 
-        for(int i = 0; i < tempArray.length; i++){
+        for (int i = 0; i < tempArray.length; i++) {
             tempArrayList.add(Integer.parseInt(tempArray[i]));
         }
 
@@ -85,13 +106,18 @@ class OptimalPage {
     }
 
 
-
-
-    private String[] populateArray(int targetFrameSize){
+    /**
+     * Takes "frameSize" Entered By User And Fills
+     * Every Index Of "tempArray" with Asterisks
+     *
+     * @param targetFrameSize "frameSize" Entered By User
+     * @return                Copies Contents Into "memArray"
+     */
+    private String[] populateArray(int targetFrameSize) {
 
         String[] tempArray = new String[targetFrameSize];
 
-        for(int i = 0; i < tempArray.length ; i++){
+        for (int i = 0; i < tempArray.length; i++) {
             tempArray[i] = "*";
         }
 
@@ -100,9 +126,32 @@ class OptimalPage {
     }
 
 
-
-
-    void initialize(){
+    /**
+     * ONLY METHOD To Ask/Recieve USER INPUT
+     * <p>
+     * Initializes Class Variables...
+     * <p>
+     * "refString",
+     * "refArrayList",
+     * "frameSize",
+     * "memArray",
+     * <p>
+     * And Uses...
+     * <p>
+     * "numericStringValidation()"
+     * "populateArrayList()"
+     * "populateArray()"
+     * <p>
+     * Methods To Achieve The Following...
+     * <p>
+     * "refString" -> INITIALIZE -> "refArrayList"
+     * "frameSize" -> INITIALIZE -> "memArray"
+     * <p>
+     * Method Is Called Within Main Class.
+     *
+     * @return Nothing
+     */
+    void initialize() {
 
         System.out.println("Enter A Numerical Reference String");
 
@@ -113,23 +162,29 @@ class OptimalPage {
         refArrayList = populateArrayList(refString);
 
 
-
         System.out.println("\nEnter The # Of Frames");
 
         //dataValidation Ensures Integer.parseInt() Will Not Throw An Exception
         frameSize = Integer.parseInt(numericStringValidation("Frame"));
         memArray = populateArray(frameSize);
 
+        keyboard.close();
+
     }
 
 
-
-
-    private String displayArray(String[] targetArray, int targetFrames){
+    /**
+     * Displays Contents Of "memArray" To User, To Be Used Within Print Statement.
+     *
+     * @param  targetArray  Used As A Temporary Array To Copy Contents Into "memArray"
+     * @param  targetFrames Used As A Max Limit To Reach Last Index Of "targetArray"
+     * @return tempString   Used To Embed Contents Of Current Array In A Print Statement
+     */
+    private String displayArray(String[] targetArray, int targetFrames) {
 
         String tempString = "";
 
-        for(int i = 0; i < targetFrames; i++){
+        for (int i = 0; i < targetFrames; i++) {
             tempString += targetArray[i];
         }
 
@@ -137,33 +192,149 @@ class OptimalPage {
     }
 
 
-//Almost Done, Fix Paramters and Javadoc Annotations
+    /**
+     * If The Current Reference Variable Within "refArrayList" Is Found Within ANY Index
+     * Of "memArray"...Return True [No Page Fault], Else Return False [Page Fault].
+     * If Index At "memArray" Is Not A Numerical Value [Page Fault].
+     *
+     * @param  targetArrayList Used As A Placeholder For "refArrayList"
+     * @param  targetArray     Used As A Placeholder For "memArray"
+     * @param  currentCount    Used As A Placeholder For "i" Within "simulate() -> checkForFault()"
+     * @return isEqual         Used Within "checkForFault()" Method To Determine If Page Fault Occurred
+     */
+    private boolean currentRefEquals(ArrayList<Integer> targetArrayList, String[] targetArray, int currentCount) {
 
-    private void mutateArray(String[] targetArray, ArrayList<Integer> targetArrayList, int currentCount){
+        boolean isEqual = false;
 
-        if(currentCount < memArray.length){
+        for (int i = 0; i < frameSize; i++) {
 
-            targetArray[currentCount] = targetArrayList.get(currentCount).toString();
+            if (targetArrayList.get(currentCount).toString().equals(targetArray[i])
+                    & targetArray[currentCount % frameSize].matches("[0-9]")) {
+
+                isEqual = true;
+
+            }
+
+        }
+
+        return isEqual;
+
+    }
+
+
+    /**
+     * Called Within "checkForFault()" Method To Handle Page Fault. Prior To Execution Program Knows
+     * There Is A Page Fault. First Portion Of "if...else" Block Replaces Any Non Numerical String
+     * Within "memArray" With Numerical Value From "refArrayList". Second Portion Of "if...else" Block
+     * Uses Personal Solution To "Optimal Page Replacement Algorithm".
+     * <p>
+     * Effectively, The Current Values Across All Index's Of "memArray" Are Compared Against The
+     * Remaining Values Of "refArrayList" To Calculate The Index And Value Within "memArray" That
+     * Will Not Be Seen For The Longest Period Of Time. This Index Is Replaced By The Current
+     * Value Of "refArrayList".
+     *
+     * @param  targetArray     Used As A Placeholder For "memArray"
+     * @param  targetArrayList Used As A Placeholder For "refArrayList"
+     * @param  currentCount    Used As A Placeholder For "i" Within "simulate() -> checkForFault()"
+     * @return Nothing
+     */
+    private void fault(String[] targetArray, ArrayList<Integer> targetArrayList, int currentCount) {
+
+        //Current value in array must be a numerical value
+        if (!targetArray[currentCount % frameSize].matches("[0-9]")) {
+
+            targetArray[currentCount % frameSize] = targetArrayList.get(currentCount).toString();
+
+        } else {
+            //Run algorithm to manipulate array index to replace with largest traversal
+
+            String currentVal = targetArrayList.get(currentCount).toString();
+
+            int traversals = 0;
+            int length = 0;
+            int largestTraversal = -1;
+            int indexOfLargest = 0;
+            int oldCount = currentCount;
+
+            //Cycles Through Each Element Of "memArray"
+            while (length < frameSize) {
+
+                //Counts Number Of Traversals For Each Element In "refArrayList"
+                for (; currentCount < targetArrayList.size(); currentCount++) {
+
+                    if (!targetArrayList.get(currentCount).toString().equals(targetArray[length])) {
+                        traversals++;
+                    } else {
+
+                        if (largestTraversal < traversals) {
+                            largestTraversal = traversals;
+                            indexOfLargest = length;
+                        }
+
+                        continue;
+
+                    }
+
+                }//END FOR LOOP
+
+                length++;
+                currentCount = oldCount;
+                traversals = 0;
+
+            }//END WHILE LOOP
+
+            //Changes Index Of "memArray" With Largest Traversal [Value That Is Not Seen For Longest Time]
+            targetArray[indexOfLargest] = currentVal;
+
+        }
+
+    }
+
+
+    /**
+     * Called From Within "simulate()" Method. This Method Uses The "fault()" Method To
+     * Determine If There Is A Page Fault. This Method Also Tracks And Increments "pageFaults",
+     * And "currentCount" Variables. "currentCount" Is Derived From "i" Within The "simulate()"
+     * Method Which Cycles Through All Elements Of "refArrayList". User Is Notified of...
+     * Current Value Within "refArrayList" -> Current Contents Of "memArrayList" As Well
+     * As If A Page Fault Occurred Or If There Was A Hit [Number Already Existed Within "memArray"].
+     * The Current Number Of "pageFaults" Is Then Displayed To The User.
+     *
+     * @param  targetArray     Used As A Placeholder For "memArray"
+     * @param  targetArrayList Used As A Placeholder For "refArrayList"
+     * @param  currentCount    Used As A Placeholder For "i" Within "simulate()"
+     * @return description
+     */
+    private void checkForFault(String[] targetArray, ArrayList<Integer> targetArrayList, int currentCount) {
+
+
+        if (!currentRefEquals(targetArrayList, targetArray, currentCount)) {
+
+            //Current index in refArrayList != Any # in Array
+            fault(memArray, refArrayList, currentCount);
 
             pageFaults++;
 
-            System.out.println(refArrayList.get(currentCount)
+            System.out.println(targetArrayList.get(currentCount)
                     + ": Memory is: "
-                    + displayArray(memArray, frameSize)
+                    + displayArray(targetArray, frameSize)
                     + ": Page Fault: "
                     + " (Number of Page Faults: "
-                    + pageFaults);
+                    + pageFaults
+                    + ")");
 
             currentCount++;
 
         } else {
 
-            System.out.println(refArrayList.get(currentCount)
+            //Current index in refArrayList == Any # in Array
+            System.out.println(targetArrayList.get(currentCount)
                     + ": Memory is: "
                     + displayArray(memArray, frameSize)
                     + ": Hit: \t\t"
                     + "(Number of Page Faults: "
-                    + pageFaults);
+                    + pageFaults
+                    + ")");
 
             currentCount++;
 
@@ -171,42 +342,26 @@ class OptimalPage {
 
     }
 
-
-
-
-    void simulate(){
+    /**
+     * Traverses "refArrayList" And Notifies Users Of Page Fault or Hit For Each Element
+     * Found Within "refArrayList". Method Is Called Within "Main" Class.
+     *
+     * @return Nothing
+     */
+    void simulate() {
 
         System.out.println("\n\nRunning Simulation: \n");
-        System.out.println("Start: Memory is: " + displayArray(memArray,frameSize));
+        System.out.println("Start: Memory is: " + displayArray(memArray, frameSize));
 
-        for(int i = 0; i < refArrayList.size(); i++){
+        for (int i = 0; i < refArrayList.size(); i++) {
 
-            mutateArray(memArray, refArrayList, i);
+            checkForFault(memArray, refArrayList, i);
 
         }
+
+        System.out.println("Total Number Of Page Faults: " + pageFaults);
 
     }
 
 
-
-
 }//END OptimalPage Class
-
-
-
-
-
-//METHODS
-/**
- * Description of Method
- *
- * @param    p1            description
- * @param    p2            description
- * @return                 description
- * @throws   IOException   if...
- */
-
-//VARIABLES
-/** Description of variables */
-
-// Reference String = 70120304230321201701
